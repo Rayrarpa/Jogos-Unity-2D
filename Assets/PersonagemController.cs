03de07db972b55f731ef96518f60c41e73e8abf6
Luanzin26 <rafadevvv@gmail.com>
2026-05-29 15:58:26 -0300

    gfgfgf

---
+++ Assets/PersonagemController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemController : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float vel = 5f;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movimento = new Vector2(HorizontalInput, VerticalInput);
+
+        if (rb2d.velocity.magnitude < 5)
+        {
+            rb2d.velocity += movimento * vel;
+        }
+    }
+}
